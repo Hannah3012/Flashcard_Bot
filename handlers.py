152e8eef873c7e6d4ecf0ba98ffbbc1f1725e58e
Hanna Tariku <hannatariku1996@gmail.com>
2026-03-20 23:54:31 +0300

    feat: Implement conversation flow for adding flashcards

--- handlers.py
+++ handlers.py
@@ -2,6 +2,41 @@ from telegram import Update
 from telegram.ext import ContextTypes, ConversationHandler, CommandHandler, MessageHandler, filters
 import random
 
+#states
+QUESTION, ANSWER = range(2)
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
-    await update.message.reply_text(f"Hi {user.full_name}")
+    await update.message.reply_text(f"Hi {user.mention_html()}! welcome\n\nUse /add to create a flashcard \nUse /help to see available commands", parse_mode="HTML")
+
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Send the question: ")
+    return QUESTION
+
+async def get_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data['question'] = update.message.text
+    await update.message.reply_text("Got it! Now, send the Answer: ")
+    return ANSWER
+async def get_answer(update: Update, context: ContextTypes):
+    question = context.user_data.get('question')
+    answer = update.message.text
+    await update.message.reply_text(f"question: {question}\nAnswer: {answer}\n")
+    return ConversationHandler.END
+    
+    
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("successfully canceled!")
+    
+TEXT_ONLY = filters.TEXT & ~filters.COMMAND
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler("add", add)],
+    states = {
+        QUESTION: [MessageHandler(TEXT_ONLY, get_question)],
+        ANSWER: [MessageHandler(TEXT_ONLY, get_answer)],
+    },
+
+    fallbacks=[
+        CommandHandler('cancel', cancel)
+    ],
+)
