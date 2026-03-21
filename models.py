1b965d42784e2ca3e653caafe6a2776949aa767f
Hanna Tariku <hannatariku1996@gmail.com>
2026-03-21 19:39:12 +0300

    feat: add Flashcard model and database setup using SQLAlchemy

--- models.py
+++ models.py
@@ -1,4 +1,16 @@
-from sqlalchemy import column , Integer, String, create_engine
+from sqlalchemy import Column , Integer, String, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
+
+class Flashcards(Base):
+    __tablename__ = "Flashcards"
+    id = Column(Integer , primary_key=True)
+    user_id = Column(Integer)
+    question = Column(String)
+    answer = Column(String)
+
+engine = create_engine('sqlite://flashcards.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
