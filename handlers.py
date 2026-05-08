b0a8034f6676eb1e017ff4d69a90b39486c291ea
Hanna Tariku <hannatariku1996@gmail.com>
2026-05-08 11:51:40 +0300

    feat: allow users to delete saved cards

--- handlers.py
+++ handlers.py
@@ -6,11 +6,11 @@ from models import Flashcard, Session
 #states
 QUESTION, ANSWER = range(2)
 QUIZ_ANSWER = 2
-
+CARD_TO_DELETE = 1
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
-    await update.message.reply_text(f"Hi {user.mention_html()}! welcome\n\nUse /add to create a flashcard \nUse /help to see available commands", parse_mode="HTML")
+    await update.message.reply_text(f"Hi {user.mention_html()}! welcome\n\nUse /help to see available commands", parse_mode="HTML")
 
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Send the question: ")
@@ -32,7 +32,7 @@ async def get_answer(update: Update, context: ContextTypes):
             question = question,
             answer = answer
         )
-
+ 
         session.add(new_card)
         session.commit()
 
@@ -96,6 +96,50 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/review - Test yourself with a random card\n"
         "/cancel - Stop the current action\n", parse_mode="HTML"
     )
+
+async def list_to_delete(update: Update, context: ContextTypes):
+    user_id = update.effective_user.id
+    session = Session()
+    cards = session.query(Flashcard).filter_by(user_id = user_id).all()
+    session.close()
+
+    if not cards:
+        await update.message.reply_text("You have no cards to delete.")
+        return ConversationHandler.END
+    
+    context.user_data['card_ids'] = [card.id for card in cards]
+
+    message = "<b>Choose the number of the card to delete: </b>\n\n"
+
+    for i, card in enumerate(cards, 1):
+        message += f"<b>{i}</b>: Question: {card.question} \n Answer: {card.answer}\n\n"
+    
+    await update.message.reply_text(message, parse_mode="HTML")
+    return CARD_TO_DELETE
+
+async def delete(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    card_ids = context.user_data.get('card_ids')
+    try:
+        user_input = int(update.message.text) -1
+        if 0 <= user_input < len(card_ids):
+            card_id = card_ids[user_input]
+            session = Session()
+            card_to_delete = session.query(Flashcard).get(card_id)
+
+            if card_to_delete:
+                session.delete(card_to_delete)
+                session.commit()
+                await update.message.reply_text("Card deleted.")
+                session.close()
+        else:
+            await update.message.reply_text(f"No card found with this number, Please choose a number between 1 and {len(card_ids)}. ")
+            return CARD_TO_DELETE # keeps the user in the state to try again
+        
+    except(ValueError, TypeError):
+        await update.message.reply_text("Please enter a valid number.")
+
+    return ConversationHandler.END
+
 TEXT_ONLY = filters.TEXT & ~filters.COMMAND
 Flashcard_conv = ConversationHandler(
     entry_points=[
